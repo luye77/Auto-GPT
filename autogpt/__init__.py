603a6462735d79bc5c759d74d90f0f67d8da8a04
bobo <1554612491@qq.com>
2023-05-29 19:58:21 +0800

    init for linux

--- autogpt/__init__.py
+++ autogpt/__init__.py
@@ -1,6 +1,7 @@
 import os
 import random
 import sys
+import os
 
 from dotenv import load_dotenv
 
@@ -10,5 +11,10 @@ if "pytest" in sys.argv or "pytest" in sys.modules or os.getenv("CI"):
 
 # Load the users .env file into environment variables
 load_dotenv(verbose=True, override=True)
+
+os.environ["http_proxy"]="192.168.31.14:7890"
+# os.environ["http_proxy"]="127.0.0.1:7890"
+os.environ["https_proxy"]="192.168.31.14:7890"
+# os.environ["https_proxy"]="192.168.31.14:7890"
 
 del load_dotenv
